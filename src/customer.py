340c1227cfdef7c0aa4468cbbff25e22d87852ac
MistaRae <itsamistarae@gmail.com>
2021-04-07 12:00:36 +0100

    classes defined

---
+++ src/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__(self, name, wallet, drunkenness):
+        self.name = name
+        self.wallet = wallet
+        self.drunkenness = drunkenness
+    def buy_drink():
+        pass
---
+++ src/pub.py
@@ -0,0 +1,8 @@
+class Pub:
+    def __init__(self, name, till, stock):
+         self.name = name
+         self.till = till
+         self.stock = stock
+    
+    def is_customer_too_drunk(self):
+        pass
